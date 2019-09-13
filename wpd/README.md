c476fdaa05b1a5b3ff39da00e17e02708b2e8f72
tmartinez114 <tmartinez114@gmail.com>
2019-09-12 22:52:30 -0400

    added wpd dir

---
+++ wpd/README.md
@@ -0,0 +1,7 @@
+WPD Radio Tracking - built by Thomas Martinez
+
+- Will need to move MongoDB from personal account to work account
+
+- Referenced https://www.youtube.com/watch?time_continue=851&v=qvBZevK1HPo
+
+![](/logo/radio.jpg)
---
+++ wpd/mern-radio-inv-2.0/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/boostrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h2>WPD Radio Inventory</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
