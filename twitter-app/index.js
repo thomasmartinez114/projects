a6a3da42c9ab4ea101b99ff6920e38dc81a71802
Thomas Martinez <tmartinez114@gmail.com>
2020-05-25 13:50:27 -0400

    async function with try catch

--- twitter-app/index.js
+++ twitter-app/index.js
@@ -4,3 +4,19 @@ const user = new Twitter({
     consumer_key: "NAemcV2ExuYVEmG6XsTfWz7c8",
     consumer_secret: "tAJx8ENG6GlbyoHE72k1IEVmTCOtbGbEJWy25OWrSyQhfJCPNn",
 });
+
+(async function() {
+    try {
+        // Retrieve bearer token from twitter
+        const response = await user.getBearerToken();
+        console.log(`Retrieved the Bearer token from twitter: ${response.access_token}`);
+
+        // Construct API client with the bearer token.
+        const app = new Twitter({
+            bearer_token: response.access_token,
+        });
+    } catch(e) {
+        console.log("There was an error calling the Twitter API.");
+        console.dir(e);
+    }
+})();
