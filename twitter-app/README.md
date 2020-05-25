e12366bd67372e04b5bb29068026e9be22a4ca33
Thomas Martinez <tmartinez114@gmail.com>
2020-05-25 13:43:38 -0400

    added the Twitter API keys and installed Twitter-lite

--- twitter-app/README.md
+++ twitter-app/README.md
@@ -1,1 +1,3 @@
 Search words and find recent tweets that contain the word.
+
+- Install **Twitter-lite**. Interaction with Twitter API tool.
---
+++ twitter-app/index.js
@@ -0,0 +1,6 @@
+const Twitter = require('twitter-lite');
+
+const user = new Twitter({
+    consumer_key: "NAemcV2ExuYVEmG6XsTfWz7c8",
+    consumer_secret: "tAJx8ENG6GlbyoHE72k1IEVmTCOtbGbEJWy25OWrSyQhfJCPNn",
+});
