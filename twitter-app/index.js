476e49c43927af7c870e2cbb0db5e8ee464c14b9
Thomas Martinez <tmartinez114@gmail.com>
2020-05-25 15:59:20 -0400

    added the search function - by search term, language and count

--- twitter-app/index.js
+++ twitter-app/index.js
@@ -1,20 +1,30 @@
 const Twitter = require('twitter-lite');
 
-const user = new Twitter({
-    consumer_key: "NAemcV2ExuYVEmG6XsTfWz7c8",
-    consumer_secret: "tAJx8ENG6GlbyoHE72k1IEVmTCOtbGbEJWy25OWrSyQhfJCPNn",
-});
 
 (async function() {
-    try {
-        // Retrieve bearer token from twitter
-        const response = await user.getBearerToken();
-        console.log(`Retrieved the Bearer token from twitter: ${response.access_token}`);
+    const user = new Twitter({
+        consumer_key: "NAemcV2ExuYVEmG6XsTfWz7c8",
+        consumer_secret: "tAJx8ENG6GlbyoHE72k1IEVmTCOtbGbEJWy25OWrSyQhfJCPNn",
+    });
 
+    try {
+        let response = await user.getBearerToken();
         // Construct API client with the bearer token.
         const app = new Twitter({
             bearer_token: response.access_token,
         });
+
+        // Search for recent tweets from the twitter API
+        response = await app.get(`/search/tweets`, {
+            q: "Michael Jordan", // Search term
+            lang: "en",
+            count: 100,
+        });
+
+        // Retrieve bearer token from twitter
+        const response = await user.getBearerToken();
+        console.log(`Retrieved the Bearer token from twitter: ${response.access_token}`);
+
     } catch(e) {
         console.log("There was an error calling the Twitter API.");
         console.dir(e);
