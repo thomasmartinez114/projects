adf62a40a69b6a1dd4618d8264fd63faf2d2995f
Thomas Martinez <tmartinez114@gmail.com>
2020-08-05 12:01:27 -0400

    HTML form to input new record

--- gnie/php-beginner-crud-level-1/config/create.php
+++ gnie/php-beginner-crud-level-1/config/create.php
@@ -16,7 +16,32 @@
             <h1>Create Product</h1>
         </div>
       
-    <!-- html form to create product will be here -->
+<!-- PHP insert code will be here -->
+ 
+<!-- html form here where the product information will be entered -->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <table class='table table-hover table-responsive table-bordered'>
+        <tr>
+            <td>Name</td>
+            <td><input type='text' name='name' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td><textarea name='description' class='form-control'></textarea></td>
+        </tr>
+        <tr>
+            <td>Price</td>
+            <td><input type='text' name='price' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type='submit' value='Save' class='btn btn-primary' />
+                <a href='index.php' class='btn btn-danger'>Back to read products</a>
+            </td>
+        </tr>
+    </table>
+</form>
           
     </div> <!-- end .container -->
       
