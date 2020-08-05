06ab09a1ac76a5ef22099efae0a9f48364e87dd0
Thomas Martinez <tmartinez114@gmail.com>
2020-08-05 12:58:10 -0400

    removed unneeded file information

--- gnie/php-beginner-crud-level-1/config/create.php
+++ gnie/php-beginner-crud-level-1/config/create.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>GNIE Create a Record</title>
+    <title>GNIE - Costa Rica | Upload</title>
       
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
@@ -13,7 +13,7 @@
     <div class="container">
    
         <div class="page-header">
-            <h1>Create Product</h1>
+            <h1>Upload a File</h1>
         </div>
       
 <?php
@@ -25,20 +25,16 @@ if($_POST){
     try{
      
         // insert query
-        $query = "INSERT INTO files_cr_cc SET fileName=:fileName, site=:site, created=:created, modified=:modified";
+        $query = "INSERT INTO files_cr_cc SET fileName=:fileName";
  
         // prepare query for execution
         $stmt = $con->prepare($query);
  
         // posted values
         $fileName=htmlspecialchars(strip_tags($_POST['fileName']));
-        $site=htmlspecialchars(strip_tags($_POST['site']));
-        $created=htmlspecialchars(strip_tags($_POST['created']));
  
         // bind the parameters
         $stmt->bindParam(':fileName', $fileName);
-        $stmt->bindParam(':site', $site);
-        $stmt->bindParam(':created', $created);
          
         // specify when this record was inserted to the database
         $modified=date('Y-m-d H:i:s');
@@ -64,22 +60,13 @@ if($_POST){
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <table class='table table-hover table-responsive table-bordered'>
         <tr>
-            <td>fileName</td>
+            <td>File Name</td>
             <td><input type='text' fileName='fileName' class='form-control' /></td>
         </tr>
-        <tr>
-            <td>site</td>
-            <td><textarea fileName='site' class='form-control'></textarea></td>
-        </tr>
-        <tr>
-            <td>created</td>
-            <td><input type='text' fileName='created' class='form-control' /></td>
-        </tr>
-        <tr>
             <td></td>
             <td>
                 <input type='submit' value='Save' class='btn btn-primary' />
-                <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                <a href='index.php' class='btn btn-danger'>Back to view Files</a>
             </td>
         </tr>
     </table>
