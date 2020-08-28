b90e0f287328885927e4b91c4b0595096d1db219
Thomas Martinez <tmartinez114@gmail.com>
2020-08-28 00:19:42 -0400

    Fetching MySQL data

--- gnie/php-beginner-crud-level-1/templates/modals/read-modal.php
+++ gnie/php-beginner-crud-level-1/templates/modals/read-modal.php
@@ -1,30 +1,43 @@
+<?php
+
+    include('tableName.php');
+    include('../database.php');
+
+    $connection = mysqli_connect($host, $username, $password);
+    $db = mysqli_select_db($connection, $db_name);
+
+    $query = "SELECT * FROM $tableName";
+    $query_run = mysqli_query($connection, $query);
+?>
+
 <table class="table table-striped">
     <thead>
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">File Name</th>
         </tr>
     </thead>
+
+    <?php
+    if($query_run)
+    {
+        foreach($query_run as $row)
+        {
+    ?>
     <tbody>
         <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
+            <td> <?php echo $row['id'] ?></td>
+            <td> <?php echo $row['fileName'] ?></td>
         </tr>
     </tbody>
+    <?php
+        }
+    }
+    else
+    {
+        echo "No Record Found";
+    }
+
+?>
+
 </table>
