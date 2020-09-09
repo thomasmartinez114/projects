a5598d0139e633f7e2e2369a63b153c3e08e1e69
Thomas Martinez <tmartinez114@gmail.com>
2020-09-09 17:09:18 -0400

    DELETE - file moved to archive

--- gnie/file_server/sites/CR/deleteCode.php
+++ gnie/file_server/sites/CR/deleteCode.php
@@ -10,18 +10,48 @@ if(isset($_POST['deleteFile']))
 {
     $id = $_POST['delete_id'];
 
-    // Remove file from MySQL DB
-    $query = "DELETE FROM $tableName WHERE id='$id'";
-    $query_run = mysqli_query($connection, $query);
+    {
+        $query_delete = "SELECT * FROM $tableName WHERE id='$id'";
+        $query_delete_run = mysqli_query($connection, $query_delete);
 
-    if($query_run)
-    {
-        echo '<script> alert("File Removed"); </script>';
-        header("location: index.php");
+        foreach($query_delete_run as $row){
+        
+        // Path desired
+        $target_dir = '../../uploads/CR/archive/';
+
+        // Current Full File Name
+        $fullName = $row['fullName'];
+        
+
+        // Trim the path from the $fullName
+        $removeDir = '../../uploads/CR/';
+        $trimmedPath = substr($fullName, 17);
+        // echo $trimmedPath;
+
+        // Set new path
+        $newPath = $target_dir.$trimmedPath;
+        // echo $newPath;
+
+        // Move File
+        $moveFile = rename($fullName, $newPath);
+
+        }
     }
-    else
+    
     {
-        echo '<script> alert("File Was Not Removed"); </script>';
+        // Remove file from MySQL DB
+        $query = "DELETE FROM $tableName WHERE id='$id'";
+        $query_run = mysqli_query($connection, $query);
+
+        if($query_run)
+        {
+            echo '<script> alert("File Removed"); </script>';
+            header("location: index.php");
+        }
+        else
+        {
+            echo '<script> alert("File Was Not Removed"); </script>';
+        }
     }
     
 }
