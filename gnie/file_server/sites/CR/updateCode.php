fd4c8aa94c28f86fd57ba9ebf5f4617f046bba05
Thomas Martinez <tmartinez114@gmail.com>
2020-09-11 17:29:40 -0400

    UPDATE - path was correct to DB just was not overwriting file

--- gnie/file_server/sites/CR/updateCode.php
+++ gnie/file_server/sites/CR/updateCode.php
@@ -10,94 +10,67 @@ $db = mysqli_select_db($connection, $db_name);
 
 ///////////////////////////////////////////////////////////////////////
 
-$target_dir = "../../uploads/cr/";
-$target_file = $target_dir . basename($_FILES["updateFile"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-///////////////////////////////////////////////////////////////////////
-
-if(isset($_POST['updateFile'])){
-
-        // ID record of upload
+if(isset($_POST['updateFile']))
+{
     $id = $_POST['update_id'];
-        // name of file
-    $fileName = ltrim($_POST['fileName']);
-        // get client username
-    $modifiedBy = substr(strrchr($_SERVER['AUTH_USER'], '\\'), 1);
-        // specify when this record was inserted to the database
-    $modified = date('Y-m-d H:i:s');
 
-    $query = "UPDATE $tableName SET fileName = '$fileName', modifiedBy = '$modifiedBy', modified = '$modified' WHERE id = '$id'";
-    $query_run = mysqli_query($connection, $query);
+    {
+        $query_update = "SELECT * FROM $tableName WHERE id = '$id'";
+        $query_update_run = mysqli_query($connection, $query_update);
 
-    if($query_run) {
-        
-        echo '<script> alert("File Updated"); </script>';
-        header("location: index.php");
-    
-    }
-    
-    else {
-        
-        echo '<script> alert("File Not Updated"); </script>';
-    
-    }
-
-///////////////////////////////////////////////////////////////////////
-
-    if($check !== false) {
-  
-        // echo "File is an image - " . $check["mime"] . ".";
+        $target_dir = "../../uploads/cr/";
+        $target_file = $target_dir . basename($_FILES["fileEdit"]["name"]);
+        echo "<br>";
+        echo "Target File: ".$target_file;
+        echo "<br>";
         $uploadOk = 1;
-      
-      } 
-      
-        else {
-    
-        // echo "File is not an image.";
-        $uploadOk = 0;
-      
-      }
-    }
-    
-      // Check if file already exists
-    if (file_exists($target_file)) {
-      
-      echo "Sorry, file already exists.";
-      $uploadOk = 0;
-    
-    }
-    
-      // Check file size
-    if ($_FILES["fileUpload"]["size"] > 500000) {
-    
-      echo "Sorry, your file is too large.";
-      $uploadOk = 0;
-    
-    }
-    
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-    
-      echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    
-    } 
-    
-    else {
-    
-      if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
-      
-        echo "The file ". basename( $_FILES["fileUpload"]["name"]). " has been uploaded.";
-      
-      } 
-      
-      else {
-      
-        echo "Sorry, there was an error uploading your file.";
-      
-      }
-}
 
+        foreach($query_update_run as $row){
+
+        // Current Full File Name
+        $fullName = $row['fullName'];
+        
+
+        // Trim the path from the $fullName
+        // $removeDir = '../../uploads/cr/';
+        $trimmedPath = substr($fullName, 17);
+        echo "<br>";
+        echo "Trimmed Path: ".$trimmedPath;
+        echo "<br>";
+
+        // Set new path
+        $newPath = $target_dir.$trimmedPath;
+        echo "<br>";
+        echo "New Path: ".$newPath;
+        echo "<br>";
+
+        // Move File
+        $moveFile = rename($fullName, $newPath);
+
+                // name of file
+        $fileName = ltrim($_POST['fileName']);
+
+                    // get client username
+        $modifiedBy = substr(strrchr($_SERVER['AUTH_USER'], '\\'), 1);
+
+            // specify when this record was inserted to the database
+        $modified = date('Y-m-d H:i:s');
+
+        $query = "UPDATE $tableName SET fileName = '$fileName', fullName = '$newPath', modifiedBy = '$modifiedBy', modified = '$modified' WHERE id = '$id'";
+        $query_run = mysqli_query($connection, $query);
+
+        }
+
+        if($query_update_run)
+        {
+            echo '<script> alert("File Update"); </script>';
+            // header("location: index.php");
+        }
+        else
+        {
+            echo '<script> alert("File Was Not Update"); </script>';
+        }
+    }
+    
+}
 ?>
