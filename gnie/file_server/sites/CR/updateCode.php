b2e8c80d62ece4ac5178e540db5f9af9cdd46df5
Thomas Martinez <tmartinez114@gmail.com>
2020-09-13 23:58:01 -0400

    UPDATE: fullName and fileName update but new file does not upload

--- gnie/file_server/sites/CR/updateCode.php
+++ gnie/file_server/sites/CR/updateCode.php
@@ -9,7 +9,7 @@ $connection = mysqli_connect($host, $username, $password);
 $db = mysqli_select_db($connection, $db_name);
 
 $target_dir = "../../uploads/cr/";
-$target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+// $target_file = $target_dir . basename($_FILES["fileName"]["name"]);
 $uploadOk = 1;
 
 ///////////////////////////////////////////////////////////////////////
@@ -24,7 +24,7 @@ if(isset($_POST['updateFile']))
 
             // Loop through Database Row
             foreach($query_update_run as $row)
-            {
+        {
 
             // Move the original file to archive folder
             $target_archive_dir = '../../uploads/cr/archive/';
@@ -43,34 +43,41 @@ if(isset($_POST['updateFile']))
             // unlink($fullName);
             $moveFile = rename($fullName, $archivePath);
 
-            
-        }
-            // // Upload the file that will overwrite the previous
+    ////////////////////////////////////////////////////////////////////
+    //                  Upload file with the same name                //
+    ////////////////////////////////////////////////////////////////////
+    
+            // Upload path
+            $uploadPath = '../../uploads/cr/';
 
-            // // name of file
-            // $fileName = ltrim($_POST['fileName']);
-            // // echo $fileName;
+            // Set file name and ltrim to remove whitespace
+            $fileName = ltrim($_POST['fileName']);
 
+            // Set file path to uploads/cr/
+            $uploadPath = $uploadPath.$trimmedPath;
+            // echo "Upload path: ".$uploadPath;
+    
+            // $overwrite = $uploadPath.$trimmedPath;
+    
             // // Add new file into folder
             // $moveFile = rename($newPath, $newPath);
-
-            //     // get client username
-            // $modifiedBy = substr(strrchr($_SERVER['AUTH_USER'], '\\'), 1);
-
-            // // specify when this record was inserted to the database
-            // $modified = date('Y-m-d H:i:s');
-
-            // $query = "UPDATE $tableName SET fileName = '$fileName', fullName = '$newPath', modifiedBy = '$modifiedBy', modified = '$modified' WHERE id = '$id'";
-            // $query_run = mysqli_query($connection, $query);
-
-
-        if($query_run)
-        {
-            // redirect back to the index page
-            header('Location: index.php');
-        }
+    
+                // get client username
+            $modifiedBy = substr(strrchr($_SERVER['AUTH_USER'], '\\'), 1);
+    
+            // specify when this record was inserted to the database
+            $modified = date('Y-m-d H:i:s');
+    
+            $query = "UPDATE $tableName SET fileName = '$fileName', fullName = '$uploadPath', modifiedBy = '$modifiedBy', modified = '$modified' WHERE id = '$id'";
+            $query_run = mysqli_query($connection, $query);
+        
+            if($query_run)
+            {
+                // redirect back to the index page
+                // header('Location: index.php');
+            }
+        }   
     }
-
 }
 
 ?>
